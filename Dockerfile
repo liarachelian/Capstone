bfeb6d3681e8856efbc2e5b29f0073d96fb58ed2
Lee Arachelian <maliny1240@gmail.com>
2020-06-17 18:25:48 -0400

    add apk to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM nginx:stable-alpine
 
 COPY . index.html /usr/share/nginx/html/
 
+RUN apk add tidy
+
 EXPOSE 80
--- createcluster.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-eksctl create cluster \
- --name udacity-cluster \
- --version 1.16 \
- --without-nodegroup
